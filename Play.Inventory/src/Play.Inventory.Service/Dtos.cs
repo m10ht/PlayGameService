a9b57aa31fb94366c3286438fc9ee3a94a76fcc5
KhanhDang <vankhanhytf@gmail.com>
2023-07-25 01:39:49 +0700

    Add PlayInventoryService

---
+++ Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Play.Inventory.Service.Dtos
+{
+    public record GrantItemsDto(
+        Guid UserId,
+        Guid CatalogItemId,
+        int Quantity);
+
+    public record InventoryItemDto(
+        Guid CatalogItemId,
+        string Name,
+        string Description,
+        int Quantity,
+        DateTimeOffset AcquiredDate);
+
+    public record CatalogItemDto(
+        Guid Id,
+        string Name,
+        string Description,
+        decimal Price,
+        DateTimeOffset CreateDate);
+}
